4e26c99f59770536566a8e7cc81711510a748aa1
Roger Leigh <rleigh@debian.org>
2013-07-17 21:38:16 +0100

    qt/make: Add sources

---
+++ qt/make/main.cpp
@@ -0,0 +1,12 @@
+#include "ogcalc.h"
+#include <QApplication>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ogcalc w;
+    w.show();
+    
+    return a.exec();
+}
