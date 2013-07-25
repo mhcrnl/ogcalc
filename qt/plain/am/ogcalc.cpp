628e19d67315cb528dde0188b36792a2eae8e44b
Roger Leigh <rleigh@debian.org>
2013-07-25 22:33:48 +0100

    qt: Add readable plain widget names

--- qt/plain/am/ogcalc.cpp
+++ qt/plain/am/ogcalc.cpp
@@ -29,47 +29,47 @@ ogcalc::~ogcalc ()
 void
 ogcalc::setupUi ()
 {
-  QWidget *centralWidget;
-  QVBoxLayout *verticalLayout;
-  QHBoxLayout *horizontalLayout;
-  QHBoxLayout *horizontalLayout_7;
-  QHBoxLayout *horizontalLayout_8;
-  QHBoxLayout *horizontalLayout_9;
-  QHBoxLayout *horizontalLayout_2;
-  QHBoxLayout *horizontalLayout_4;
-  QHBoxLayout *horizontalLayout_5;
-  QHBoxLayout *horizontalLayout_3;
+  QWidget *toplevel_layout;
+  QVBoxLayout *vertical_layout;
+  QHBoxLayout *entry_layout;
+  QHBoxLayout *pg_layout;
+  QHBoxLayout *ri_layout;
+  QHBoxLayout *cf_layout;
+  QHBoxLayout *result_layout;
+  QHBoxLayout *og_layout;
+  QHBoxLayout *abv_layout;
+  QHBoxLayout *button_layout;
 
   if (objectName().isEmpty())
     setObjectName(QStringLiteral("ogcalc"));
   resize(407, 123);
-  QSizePolicy sp(QSizePolicy::Preferred, QSizePolicy::Preferred);
-  sp.setHorizontalStretch(0);
-  sp.setVerticalStretch(0);
-  sp.setHeightForWidth(sizePolicy().hasHeightForWidth());
-  setSizePolicy(sp);
+  QSizePolicy sp1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  sp1.setHorizontalStretch(0);
+  sp1.setVerticalStretch(0);
+  sp1.setHeightForWidth(sizePolicy().hasHeightForWidth());
+  setSizePolicy(sp1);
   setMaximumSize(QSize(16777215, 16777215));
   QIcon icon;
   icon.addFile(QStringLiteral("ogcalc.svg"), QSize(), QIcon::Normal, QIcon::Off);
   setWindowIcon(icon);
-  centralWidget = new QWidget(this);
-  centralWidget->setObjectName(QStringLiteral("centralWidget"));
-  QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Minimum);
-  sizePolicy1.setHorizontalStretch(0);
-  sizePolicy1.setVerticalStretch(0);
-  sizePolicy1.setHeightForWidth(centralWidget->sizePolicy().hasHeightForWidth());
-  centralWidget->setSizePolicy(sizePolicy1);
-  verticalLayout = new QVBoxLayout(centralWidget);
-  verticalLayout->setSpacing(6);
-  verticalLayout->setContentsMargins(11, 11, 11, 11);
-  verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-  horizontalLayout = new QHBoxLayout();
-  horizontalLayout->setSpacing(6);
-  horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
-  horizontalLayout_7 = new QHBoxLayout();
-  horizontalLayout_7->setSpacing(6);
-  horizontalLayout_7->setObjectName(QStringLiteral("horizontalLayout_7"));
-  pg_label = new QLabel(centralWidget);
+  toplevel_layout = new QWidget(this);
+  toplevel_layout->setObjectName(QStringLiteral("toplevel_layout"));
+  QSizePolicy sp2(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  sp2.setHorizontalStretch(0);
+  sp2.setVerticalStretch(0);
+  sp2.setHeightForWidth(toplevel_layout->sizePolicy().hasHeightForWidth());
+  toplevel_layout->setSizePolicy(sp2);
+  vertical_layout = new QVBoxLayout(toplevel_layout);
+  vertical_layout->setSpacing(6);
+  vertical_layout->setContentsMargins(11, 11, 11, 11);
+  vertical_layout->setObjectName(QStringLiteral("vertical_layout"));
+  entry_layout = new QHBoxLayout();
+  entry_layout->setSpacing(6);
+  entry_layout->setObjectName(QStringLiteral("entry_layout"));
+  pg_layout = new QHBoxLayout();
+  pg_layout->setSpacing(6);
+  pg_layout->setObjectName(QStringLiteral("pg_layout"));
+  pg_label = new QLabel(toplevel_layout);
   pg_label->setObjectName(QStringLiteral("pg_label"));
   QSizePolicy sizePolicy2(QSizePolicy::Minimum, QSizePolicy::Preferred);
   sizePolicy2.setHorizontalStretch(0);
@@ -78,9 +78,9 @@ ogcalc::setupUi ()
   pg_label->setSizePolicy(sizePolicy2);
   pg_label->setTextFormat(Qt::AutoText);
 
-  horizontalLayout_7->addWidget(pg_label);
+  pg_layout->addWidget(pg_label);
 
-  pg_entry = new SpinBoxEntry(centralWidget);
+  pg_entry = new SpinBoxEntry(toplevel_layout);
   pg_entry->setObjectName(QStringLiteral("pg_entry"));
   QSizePolicy sizePolicy3(QSizePolicy::Expanding, QSizePolicy::Fixed);
   sizePolicy3.setHorizontalStretch(0);
@@ -88,68 +88,68 @@ ogcalc::setupUi ()
   sizePolicy3.setHeightForWidth(pg_entry->sizePolicy().hasHeightForWidth());
   pg_entry->setSizePolicy(sizePolicy3);
 
-  horizontalLayout_7->addWidget(pg_entry);
+  pg_layout->addWidget(pg_entry);
 
 
-  horizontalLayout->addLayout(horizontalLayout_7);
+  entry_layout->addLayout(pg_layout);
 
-  horizontalLayout_8 = new QHBoxLayout();
-  horizontalLayout_8->setSpacing(6);
-  horizontalLayout_8->setObjectName(QStringLiteral("horizontalLayout_8"));
-  ri_label = new QLabel(centralWidget);
+  ri_layout = new QHBoxLayout();
+  ri_layout->setSpacing(6);
+  ri_layout->setObjectName(QStringLiteral("ri_layout"));
+  ri_label = new QLabel(toplevel_layout);
   ri_label->setObjectName(QStringLiteral("ri_label"));
   sizePolicy2.setHeightForWidth(ri_label->sizePolicy().hasHeightForWidth());
   ri_label->setSizePolicy(sizePolicy2);
 
-  horizontalLayout_8->addWidget(ri_label);
+  ri_layout->addWidget(ri_label);
 
-  ri_entry = new SpinBoxEntry(centralWidget);
+  ri_entry = new SpinBoxEntry(toplevel_layout);
   ri_entry->setObjectName(QStringLiteral("ri_entry"));
   sizePolicy3.setHeightForWidth(ri_entry->sizePolicy().hasHeightForWidth());
   ri_entry->setSizePolicy(sizePolicy3);
 
-  horizontalLayout_8->addWidget(ri_entry);
+  ri_layout->addWidget(ri_entry);
 
 
-  horizontalLayout->addLayout(horizontalLayout_8);
+  entry_layout->addLayout(ri_layout);
 
-  horizontalLayout_9 = new QHBoxLayout();
-  horizontalLayout_9->setSpacing(6);
-  horizontalLayout_9->setObjectName(QStringLiteral("horizontalLayout_9"));
-  cf_label = new QLabel(centralWidget);
+  cf_layout = new QHBoxLayout();
+  cf_layout->setSpacing(6);
+  cf_layout->setObjectName(QStringLiteral("cf_layout"));
+  cf_label = new QLabel(toplevel_layout);
   cf_label->setObjectName(QStringLiteral("cf_label"));
   sizePolicy2.setHeightForWidth(cf_label->sizePolicy().hasHeightForWidth());
   cf_label->setSizePolicy(sizePolicy2);
 
-  horizontalLayout_9->addWidget(cf_label);
+  cf_layout->addWidget(cf_label);
 
-  cf_entry = new SpinBoxEntry(centralWidget);
+  cf_entry = new SpinBoxEntry(toplevel_layout);
   cf_entry->setObjectName(QStringLiteral("cf_entry"));
   sizePolicy3.setHeightForWidth(cf_entry->sizePolicy().hasHeightForWidth());
   cf_entry->setSizePolicy(sizePolicy3);
 
-  horizontalLayout_9->addWidget(cf_entry);
+  cf_layout->addWidget(cf_entry);
 
 
-  horizontalLayout->addLayout(horizontalLayout_9);
+  entry_layout->addLayout(cf_layout);
 
 
-  verticalLayout->addLayout(horizontalLayout);
+  vertical_layout->addLayout(entry_layout);
 
-  horizontalLayout_2 = new QHBoxLayout();
-  horizontalLayout_2->setSpacing(6);
-  horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
-  horizontalLayout_4 = new QHBoxLayout();
-  horizontalLayout_4->setSpacing(6);
-  horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
-  og_label = new QLabel(centralWidget);
+  result_layout = new QHBoxLayout();
+  result_layout->setSpacing(6);
+  result_layout->setObjectName(QStringLiteral("result_layout"));
+  og_layout = new QHBoxLayout();
+  og_layout->setSpacing(6);
+  og_layout->setObjectName(QStringLiteral("og_layout"));
+  og_label = new QLabel(toplevel_layout);
   og_label->setObjectName(QStringLiteral("og_label"));
   sizePolicy2.setHeightForWidth(og_label->sizePolicy().hasHeightForWidth());
   og_label->setSizePolicy(sizePolicy2);
 
-  horizontalLayout_4->addWidget(og_label);
+  og_layout->addWidget(og_label);
 
-  og_result = new QLabel(centralWidget);
+  og_result = new QLabel(toplevel_layout);
   og_result->setObjectName(QStringLiteral("og_result"));
   QSizePolicy sizePolicy4(QSizePolicy::Expanding, QSizePolicy::Preferred);
   sizePolicy4.setHorizontalStretch(0);
@@ -163,22 +163,22 @@ ogcalc::setupUi ()
   og_result->setAlignment(Qt::AlignCenter);
   og_result->setTextInteractionFlags(Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
 
-  horizontalLayout_4->addWidget(og_result);
+  og_layout->addWidget(og_result);
 
 
-  horizontalLayout_2->addLayout(horizontalLayout_4);
+  result_layout->addLayout(og_layout);
 
-  horizontalLayout_5 = new QHBoxLayout();
-  horizontalLayout_5->setSpacing(6);
-  horizontalLayout_5->setObjectName(QStringLiteral("horizontalLayout_5"));
-  abv_entry = new QLabel(centralWidget);
+  abv_layout = new QHBoxLayout();
+  abv_layout->setSpacing(6);
+  abv_layout->setObjectName(QStringLiteral("abv_layout"));
+  abv_entry = new QLabel(toplevel_layout);
   abv_entry->setObjectName(QStringLiteral("abv_entry"));
   sizePolicy2.setHeightForWidth(abv_entry->sizePolicy().hasHeightForWidth());
   abv_entry->setSizePolicy(sizePolicy2);
 
-  horizontalLayout_5->addWidget(abv_entry);
+  abv_layout->addWidget(abv_entry);
 
-  abv_result = new QLabel(centralWidget);
+  abv_result = new QLabel(toplevel_layout);
   abv_result->setObjectName(QStringLiteral("abv_result"));
   sizePolicy4.setHeightForWidth(abv_result->sizePolicy().hasHeightForWidth());
   abv_result->setSizePolicy(sizePolicy4);
@@ -186,39 +186,39 @@ ogcalc::setupUi ()
   abv_result->setAlignment(Qt::AlignCenter);
   abv_result->setTextInteractionFlags(Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
 
-  horizontalLayout_5->addWidget(abv_result);
+  abv_layout->addWidget(abv_result);
 
 
-  horizontalLayout_2->addLayout(horizontalLayout_5);
+  result_layout->addLayout(abv_layout);
 
 
-  verticalLayout->addLayout(horizontalLayout_2);
+  vertical_layout->addLayout(result_layout);
 
-  horizontalLayout_3 = new QHBoxLayout();
-  horizontalLayout_3->setSpacing(6);
-  horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
-  quit_button = new QPushButton(centralWidget);
+  button_layout = new QHBoxLayout();
+  button_layout->setSpacing(6);
+  button_layout->setObjectName(QStringLiteral("button_layout"));
+  quit_button = new QPushButton(toplevel_layout);
   quit_button->setObjectName(QStringLiteral("quit"));
 
-  horizontalLayout_3->addWidget(quit_button);
+  button_layout->addWidget(quit_button);
 
-  reset_button = new QPushButton(centralWidget);
+  reset_button = new QPushButton(toplevel_layout);
   reset_button->setObjectName(QStringLiteral("reset"));
   reset_button->setAutoDefault(true);
 
-  horizontalLayout_3->addWidget(reset_button);
+  button_layout->addWidget(reset_button);
 
-  calculate_button = new QPushButton(centralWidget);
+  calculate_button = new QPushButton(toplevel_layout);
   calculate_button->setObjectName(QStringLiteral("calculate"));
   calculate_button->setAutoDefault(true);
   calculate_button->setDefault(true);
 
-  horizontalLayout_3->addWidget(calculate_button);
+  button_layout->addWidget(calculate_button);
 
 
-  verticalLayout->addLayout(horizontalLayout_3);
+  vertical_layout->addLayout(button_layout);
 
-  setCentralWidget(centralWidget);
+  setCentralWidget(toplevel_layout);
 #ifndef QT_NO_SHORTCUT
   pg_label->setBuddy(pg_entry);
   ri_label->setBuddy(ri_entry);
@@ -243,30 +243,27 @@ void
 ogcalc::retranslateUi ()
 {
   setWindowTitle(QApplication::translate("ogcalc", "OG & ABV calculator", 0));
-#ifndef QT_NO_TOOLTIP
+
   pg_label->setToolTip(QApplication::translate("ogcalc", "Present Gravity (density)", 0));
-#endif // QT_NO_TOOLTIP
   pg_label->setText(QApplication::translate("ogcalc", "&PG:", 0));
+
   pg_entry->setSpecialValueText(QApplication::translate("ogcalc", "0.00", 0));
   pg_entry->setPrefix(QString());
-#ifndef QT_NO_TOOLTIP
+
   ri_label->setToolTip(QApplication::translate("ogcalc", "Refractive Index", 0));
-#endif // QT_NO_TOOLTIP
   ri_label->setText(QApplication::translate("ogcalc", "R&I:", 0));
-#ifndef QT_NO_TOOLTIP
+
   cf_label->setToolTip(QApplication::translate("ogcalc", "Correction Factor", 0));
-#endif // QT_NO_TOOLTIP
   cf_label->setText(QApplication::translate("ogcalc", "C&F:", 0));
-#ifndef QT_NO_TOOLTIP
+
   og_label->setToolTip(QApplication::translate("ogcalc", "Original Gravity (density)", 0));
-#endif // QT_NO_TOOLTIP
   og_label->setText(QApplication::translate("ogcalc", "OG:", 0));
   og_result->setText(QApplication::translate("ogcalc", "OGVAL", 0));
-#ifndef QT_NO_TOOLTIP
+
   abv_entry->setToolTip(QApplication::translate("ogcalc", "Percent Alcohol By Volume", 0));
-#endif // QT_NO_TOOLTIP
   abv_entry->setText(QApplication::translate("ogcalc", "ABV %:", 0));
   abv_result->setText(QApplication::translate("ogcalc", "ABVVAL", 0));
+
   quit_button->setText(QApplication::translate("ogcalc", "&Quit", 0));
   reset_button->setText(QApplication::translate("ogcalc", "&Reset", 0));
   calculate_button->setText(QApplication::translate("ogcalc", "&Calculate", 0));
