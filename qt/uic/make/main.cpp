bab18221334c03b4cc9315c1ae4b3e5ae5341bfe
Roger Leigh <rleigh@debian.org>
2013-07-28 13:04:55 +0100

    qt/uic: Move sources to common

--- qt/uic/make/main.cpp
+++ qt/uic/make/main.cpp
@@ -1,12 +1,1 @@
-#include "ogcalc.h"
-#include <QApplication>
-#include <cstdlib>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    ogcalc w;
-    w.show();
-    
-    return a.exec();
-}
+../common/main.cpp
